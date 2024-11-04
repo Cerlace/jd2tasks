db1303b29aa04780ad6b801919be08f7bb2e5367
Cerlace <124503215+Cerlace@users.noreply.github.com>
2024-11-05 01:32:33 +0300

    Скрипт вставки данных в Person

---
+++ homework/hw3/insert_person.sql
@@ -0,0 +1,30 @@
+INSERT INTO people.person (
+        age, salary, passport, address, dateOfBirthday,
+        dateTimeCreate, timeToLunch, letter)
+VALUES (23, 2443.23, 'AB12345671', 'ул.Пушкина, дом 1',
+        '2001-07-20', sysdate(), '13:00:00',
+        'Очень длинное письмо 1');
+INSERT INTO people.person (
+    age, salary, passport, address, dateOfBirthday,
+    dateTimeCreate, timeToLunch, letter)
+VALUES (20, 1104.62, 'AB12345672', 'ул.Ленина, дом 22',
+        '2004-03-24', sysdate(), '12:00:00',
+        'Очень длинное письмо 2');
+INSERT INTO people.person (
+    age, salary, passport, address, dateOfBirthday,
+    dateTimeCreate, timeToLunch, letter)
+VALUES (19, 1907.43, 'AB12345673', 'ул.Славная, дом 12',
+        '2005-08-11', sysdate(), '11:00:00',
+        'Очень длинное письмо 3');
+INSERT INTO people.person (
+    age, salary, passport, address, dateOfBirthday,
+    dateTimeCreate, timeToLunch, letter)
+VALUES (38, 1237.82, 'AB12345674', 'ул.Красивая, дом 2',
+        '1986-01-18', sysdate(), '14:00:00',
+        'Очень длинное письмо 4');
+INSERT INTO people.person (
+    age, salary, passport, address, dateOfBirthday,
+    dateTimeCreate, timeToLunch, letter)
+VALUES (35, 700.73, 'AB12345675', 'ул.Полетная, дом 4',
+        '1989-08-22', sysdate(), '15:00:00',
+        'Очень длинное письмо 5');
