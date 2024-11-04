44366cb9d800e01913e4250808aa31e08214b27c
Cerlace <124503215+Cerlace@users.noreply.github.com>
2024-11-04 22:32:56 +0300

    Тесты класса Student

--- homework/hw2/students/src/test/java/cerlace/students/StudentTest.java
+++ homework/hw2/students/src/test/java/cerlace/students/StudentTest.java
@@ -1,4 +1,24 @@
 package cerlace.students;
 
+import cerlace.dto.Skill;
+import cerlace.strategies.StudyStrategy;
+import cerlace.utils.TalentUtil;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
 public class StudentTest {
+
+    @Test
+    void learnSkillTest() {
+        StudyStrategy studyStrategy = Mockito.mock(StudyStrategy.class);
+
+        double testTalent = 1;
+        Skill testSkill = new Skill("Test skill", 30);
+        Student student = new Student(studyStrategy);
+        student.setTalent(testTalent);
+
+        student.learnSkill(testSkill);
+
+        Mockito.verify(studyStrategy).study(testSkill.getHoursToLearn(), testTalent);
+    }
 }
