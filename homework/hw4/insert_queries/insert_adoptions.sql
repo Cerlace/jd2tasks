45ab6b50498f19b59dcb74c14d185a75f25eaf7a
Cerlace <124503215+Cerlace@users.noreply.github.com>
2024-11-05 17:23:39 +0300

    Скрипт вставки данных

---
+++ homework/hw4/insert_queries/insert_adoptions.sql
@@ -0,0 +1,14 @@
+INSERT INTO adoptions (animal_id, adopter_name, adoption_date)
+VALUES (1, 'Даниил', '2016-05-13');
+
+INSERT INTO adoptions (animal_id, adopter_name, adoption_date)
+VALUES (3, 'Александр', '2019-08-10');
+
+INSERT INTO adoptions (animal_id, adopter_name, adoption_date)
+VALUES (5, 'Павел', '2023-02-03');
+
+INSERT INTO adoptions (animal_id, adopter_name, adoption_date)
+VALUES (8, 'Лев', '2011-10-20');
+
+INSERT INTO adoptions (animal_id, adopter_name, adoption_date)
+VALUES (10, 'Екатерина', '2019-06-11');
---
+++ homework/hw4/insert_queries/insert_animals.sql
@@ -0,0 +1,29 @@
+INSERT INTO animals (animal_name, species, shelter_id)
+VALUES ('Бакс', 'Кот', null);
+
+INSERT INTO animals (animal_name, species, shelter_id)
+VALUES ('Пушок', 'Собака', 2);
+
+INSERT INTO animals (animal_name, species, shelter_id)
+VALUES ('Полоскун', 'Енот', null);
+
+INSERT INTO animals (animal_name, species, shelter_id)
+VALUES ('Хома', 'Хомяк', 3);
+
+INSERT INTO animals (animal_name, species, shelter_id)
+VALUES ('Барсик', 'Кот', null);
+
+INSERT INTO animals (animal_name, species, shelter_id)
+VALUES ('Рекс', 'Собака', 2);
+
+INSERT INTO animals (animal_name, species, shelter_id)
+VALUES ('Мотя', 'Кот', 4);
+
+INSERT INTO animals (animal_name, species, shelter_id)
+VALUES ('Соломон', 'Лошадь', null);
+
+INSERT INTO animals (animal_name, species, shelter_id)
+VALUES ('Шанди', 'Собака', 5);
+
+INSERT INTO animals (animal_name, species, shelter_id)
+VALUES ('Вжух', 'Мышь', null);
