24ee797506fc7305b5246f239559ed8ebdcbb7c3
Cerlace <124503215+Cerlace@users.noreply.github.com>
2024-11-07 14:14:53 +0300

    Maven проект и утилитарный класс JDBCResources для получения properties

---
+++ homework/hw4/jdbc/src/main/java/cerlace/Main.java
@@ -0,0 +1,11 @@
+package cerlace;
+
+/**
+ * Точка входа в приложение.
+ * Выполняет основные функции, предусмотренные задачей.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ homework/hw4/jdbc/src/test/java/cerlace/MainTest.java
@@ -0,0 +1,16 @@
+package cerlace;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Тест основных функций приложения.
+ */
+public class MainTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
